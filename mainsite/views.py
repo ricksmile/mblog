ba39e02da3b304f117e08be2d405650300db1906
ricksmile <o_dormant@hotmail.com>
2017-02-16 14:23:48 +0800

    修改settin.py 與 views.py

--- mainsite/views.py
+++ mainsite/views.py
@@ -1,14 +1,23 @@
-from django.shortcuts import render
+from django.template.loader import get_template
 from django.http import HttpResponse
+from django.shortcuts import redirect
+from datetime import datetime
 from .models import Post
 
 # Create your views here.
 def homepage(request):
+    template = get_template('index.html')
     posts = Post.objects.all()
-    post_lists=list()
-    for count,post in enumerate(posts):
-      post_lists.append("No.{}:".format(str(count+1))+str(post)+"<hr>")
-      post_lists.append("<small>"+str(post.body.encode('utf-8'))\
-      +"</small><br><br>")
-    return HttpResponse(post_lists)
+    now = datetime.now()
+    html = template.render(locals())
+    return HttpResponse(html)
 
+def showpost(request, slug):
+    template = get_template('post.html')
+    try:
+        post = Post.objects.get(slug=slug)
+        if post != None:
+            html = template.render(locals())
+            return HttpResponse(html)
+    except:
+        return redirect('/')
